93215d16db49033c316f3e8c1f870756e398b701
rodrigovcf <rodrigo.rvcf@gmail.com>
2022-07-24 17:25:29 -0300

    List Costumers - Controller

--- src/main/java/com/springrest/api/controller/CustomerController.java
+++ src/main/java/com/springrest/api/controller/CustomerController.java
@@ -1,14 +1,32 @@
 package com.springrest.api.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.springrest.api.model.Customer;
 
 @RestController
 public class CustomerController {
 	
 	@GetMapping("/customers")
-	public String testRequest() {
-		return "Test!!!";
+	public List<Customer> testRequest() {
+		var customer1 = new Customer();
+		customer1.setId(1L);
+		customer1.setName("Rodrigo");
+		customer1.setEmail("rodrigo@springrest.com");
+		customer1.setPhone("(88) 8888-8888");
+		
+		var customer2 = new Customer();
+		customer2.setId(2L);
+		customer2.setName("Pedro");
+		customer2.setEmail("pedro@springrest.com");
+		customer2.setPhone("(88) 8888-9999");
+		
+		
+		return Arrays.asList(customer1, customer2) ;
 	}
 
 }
---
+++ src/main/java/com/springrest/api/model/Customer.java
@@ -0,0 +1,17 @@
+package com.springrest.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Customer {
+
+	private Long id;
+	private String name;
+	private String email;
+	private String phone;
+	
+	
+	
+}
