9a220ce889f225066182c17c20163dca0c6b8bbf
rodrigovcf <rodrigo.rvcf@gmail.com>
2022-08-03 18:02:29 -0300

    Imple CRUD - findCustomer

--- src/main/java/com/springrest/api/controller/CustomerController.java
+++ src/main/java/com/springrest/api/controller/CustomerController.java
@@ -1,9 +1,12 @@
 package com.springrest.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springrest.api.model.Customer;
@@ -22,6 +25,27 @@ public class CustomerController {
 	}
 
 	
+	/*
+	 * Version1
+	@GetMapping("/customers/{customerId}")
+	public ResponseEntity<Customer> find(@PathVariable Long customerId) {
+		Optional<Customer> customer = customerRepository.findById(customerId);
+		
+		//If exist
+		if(customer.isPresent())
+			return ResponseEntity.ok(customer.get());
+		
+		return ResponseEntity.notFound().build();//Not exist - returns 404	
+	}
+	*/
+	
+	@GetMapping("/customers/{customerId}")
+	public ResponseEntity<Customer> findCustomer(@PathVariable Long customerId) {
+		return customerRepository.findById(customerId)
+//				.map(customer -> ResponseEntity.ok(customer))
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
+	}
 	
 	
 }
