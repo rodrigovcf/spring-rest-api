7d8226a927c56eba3b34634791e29dfd9c5ac64c
rodrigovcf <rodrigo.rvcf@gmail.com>
2022-08-03 18:16:52 -0300

    Imple CRUD - addCustomer

--- src/main/java/com/springrest/api/controller/CustomerController.java
+++ src/main/java/com/springrest/api/controller/CustomerController.java
@@ -4,22 +4,28 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springrest.api.model.Customer;
 import com.springrest.domain.repository.CustomerRepository;
 
 @RestController
+@RequestMapping("/customers")
 public class CustomerController {
 	
 	@Autowired
 	private CustomerRepository customerRepository;
 	
 	
-	@GetMapping("/customers")
+	@GetMapping
 	public List<Customer> testRequest() {
 		return customerRepository.findAll();
 	}
@@ -39,7 +45,7 @@ public class CustomerController {
 	}
 	*/
 	
-	@GetMapping("/customers/{customerId}")
+	@GetMapping("/{customerId}")
 	public ResponseEntity<Customer> findCustomer(@PathVariable Long customerId) {
 		return customerRepository.findById(customerId)
 //				.map(customer -> ResponseEntity.ok(customer))
@@ -47,5 +53,10 @@ public class CustomerController {
 				.orElse(ResponseEntity.notFound().build());
 	}
 	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Customer addCustomer(@RequestBody Customer customer) { 
+		return customerRepository.save(customer);
+	}
 	
 }
